3905015f9ff3025b04225ff41ded4d47c40ba409
abdelrhman adil <aerro1043@gmail.com>
2023-09-12 11:47:27 -0700

    1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include 'main.h'
+/**
+ * print_alphabet_x10 - funx that prints the alphabet 10 tims
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line ++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
