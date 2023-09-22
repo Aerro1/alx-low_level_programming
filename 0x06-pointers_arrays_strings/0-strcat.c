afbb488946a1fbd24f143b6ae3cbc7de76abbc4c
abdelrhman adil <aerro1043@gmail.com>
2023-09-21 19:49:30 -0700

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,8 @@
 #include<stdio.h>
 
 /**
- * _strcat - function that concatenates two strings.
+ * _strcat - function that concatenates
+ *		two strings.
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
